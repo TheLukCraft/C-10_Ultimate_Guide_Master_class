d2065df26db81779fb3aaa5de19a70d108d7046a
TheLukCraft <luk1997r@o2.pl>
2023-10-31 14:35:48 +0100

    implicit casting

---
+++ Type_conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿//sbyte
+sbyte a = 10;
+
+//int 
+int b;
+
+//not working, because higher value
+//uint b;
+
+//copy the value from "a" to "b"
+b = a;
+
+Console.WriteLine(a); //10
+Console.WriteLine(b); //10
